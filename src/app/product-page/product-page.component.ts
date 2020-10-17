3a5b3b8d76a0d908b6a1053f5cb8d9d96ae84086
idan00912@gmail.com <idan00912@gmail.com>
2020-10-17 23:19:20 +0300

    Completed module 5

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { from } from "rxjs/observable/from";
+import { Album } from "../album";
+
 
 @Component({
-  selector: 'app-product-page',
-  templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.css']
+  selector: "app-product-page",
+  templateUrl: "./product-page.component.html",
+  styleUrls: ["./product-page.component.css"],
 })
 export class ProductPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
