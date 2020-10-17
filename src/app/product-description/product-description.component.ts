cfa19e18742484eeb829a763143b4d10c37d8fa8
idan00912@gmail.com <idan00912@gmail.com>
2020-10-17 15:56:52 +0300

    Completed module 4

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 import { from } from "rxjs/observable/from";
 import { ProductService } from "../product.service";
-
+import { Album } from "../album";
 @Component({
   selector: "app-product-description",
   templateUrl: "./product-description.component.html",
   styleUrls: ["./product-description.component.css"],
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo: any;
+  albumInfo: Album;
   constructor(private _productService: ProductService) {}
   ngOnInit() {
     this._productService
       .getAlbum(1)
-      .subscribe(response => this.albumInfo = response);
+      .subscribe((response) => (this.albumInfo = response));
   }
 }
