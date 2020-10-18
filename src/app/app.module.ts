efb5842388adf16b45d8484d876b92db9180c2e4
idan00912@gmail.com <idan00912@gmail.com>
2020-10-18 20:35:18 +0300

    Completed module 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,16 @@ import { ProductService } from "./product.service";
 import { AppComponent } from "./app.component";
 import { ProductPageComponent } from "./product-page/product-page.component";
 import { ProductDescriptionComponent } from "./product-description/product-description.component";
-import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductTracklistingComponent } from "./product-tracklisting/product-tracklisting.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { RouterModule, Routes } from "@angular/router";
+import { from } from "rxjs/observable/from";
+
+const appRoutes: Routes = [
+  {path: "products",component: ProductListComponent},
+  {path: "product/:id",component: ProductPageComponent},
+  {path: "",redirectTo: "products", pathMatch: "full"}
+];
 
 @NgModule({
   declarations: [
@@ -19,7 +27,12 @@ import { ProductListComponent } from './product-list/product-list.component';
     ProductTracklistingComponent,
     ProductListComponent,
   ],
-  imports: [BrowserModule, FormsModule, HttpModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [ProductService],
   bootstrap: [AppComponent],
 })
